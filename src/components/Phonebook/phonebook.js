4c79bee8b64f1f3e55a411bf3b4b5393ba3a333b
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-07-24 00:13:10 +0200

    Update phonebook.js

--- src/components/Phonebook/phonebook.js
+++ src/components/Phonebook/phonebook.js
@@ -56,5 +56,14 @@ class Phonebook extends Component {
   }
 }
 
-
+Phonebook.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  filter: PropTypes.string,
+};
 export default Phonebook;
